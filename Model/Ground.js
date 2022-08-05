9453bb9665eb29b4e190892b2336bcbda6176142
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2022-08-05 17:31:21 -0300

    classe Terreno implementada

--- Model/Ground.js
+++ Model/Ground.js
@@ -1,1 +1,12 @@
-
+class Ground{
+ 
+    constructor(x,y){
+        this.ground=[];
+        for(let i=0; i<x; i++){
+            this.ground[i]=[];
+            for(let j=0; j<y; j++){
+                this.ground[i][j]='';
+            }
+          }
+    } 
+}
