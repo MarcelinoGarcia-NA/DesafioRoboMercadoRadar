999e3f54f7f9d3a87160981ced70577ce553772b
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2022-08-05 19:15:51 -0300

    refatorando, colocando nova função

--- View/main.js
+++ View/main.js
@@ -1,5 +1,21 @@
-let c= document.getElementById('command-submit');
-let _robot= new Robot(0,0,'N');
-let _ground = new Ground(5,5);
+let form = document.getElementById('form-robot');
+let command_submit= document.getElementById('command-submit');
+ 
+form.addEventListener('submit', function(e) {
+    
+    let _robot= new Robot(0,0,'N');
+    let _ground = new Ground(5,5);
 
-console.log(PositionRobot(_robot,_ground,c));
+    let position =PositionRobot(_robot,_ground,command_submit.value);
+    document.getElementById('location-robot').textContent=position;
+
+    e.preventDefault();
+});
+
+
+
+
+
+
+
+
