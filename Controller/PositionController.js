7cb2688f8a42e28ef4e435f9e09666b1434d7652
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2022-08-05 19:14:15 -0300

    refatorando, colocando nova função

--- Controller/PositionController.js
+++ Controller/PositionController.js
@@ -1,21 +1,14 @@
-let _robot= new Robot(0,0,'N');
-let _ground = new Ground(5,5);
-let c ="MML";
 
 function PositionRobot(robot,ground,command){
-    let c = command.toUpperCase();
-    let newcommand= c.split('');
-    let cont=0;
-    for(i=0;i<newcommand.length;i++){
-        if((newcommand[i]!="M")&&(newcommand[i]!="L")&&(newcommand[i]!="R")){
-            return "Comando não é válido!";
-        }
-    }
+    let commadUpperCase = command.toUpperCase();//colocando comando em caixa alta;
+    let newcommand= commadUpperCase.split('');//criando um array com a palavra comando;
+
     try{
-        while (cont<=newcommand.length){
-            if(newcommand[cont]=="M"){
+       for(let i=0; i<newcommand.length;i++){
+           switch(newcommand[i]){
+              case 'M':
                 if(robot.getPointer=="N"){
-                    ground.ground[robot.getPosition_x][robot.getPosition_y]="";  
+                   ground.ground[robot.getPosition_x][robot.getPosition_y]="";  
                    robot.setPosition_y=robot.getPosition_y+1;
                    ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
                 }
@@ -34,7 +27,8 @@ function PositionRobot(robot,ground,command){
                     robot.setPosition_x=robot.getPosition_x+1;
                     ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
                 }
-            }if(newcommand[cont]=="L"){
+                break;
+            case 'L':
                 if(robot.getPointer=="N"){
                     robot.setPointer="W";
                     ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
@@ -51,8 +45,8 @@ function PositionRobot(robot,ground,command){
                     robot.setPointer="S";
                     ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
                 }
-            }
-            if(newcommand[cont]=="R"){
+                break;
+            case 'R':
                 if(robot.getPointer==="N"){
                     robot.setPointer="E";
                     ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
@@ -69,21 +63,23 @@ function PositionRobot(robot,ground,command){
                     robot.setPointer="N";
                     ground.ground[robot.getPosition_x][robot.getPosition_y]="Robo->"+robot.getPointer;
                 }
-                
-            }
-            cont++;
+                break;
+            default:
+                return "Comando Inválido!";
+		    }
             if((robot.getPosition_x>4 || robot.getPosition_y>4 || robot.getPosition_y<0)){
                 ground.ground[robot.getPosition_x][robot.getPosition_y]="";  
-                return "Comando invalido, com este comando o robô irá para fora do terreno";
-            }      
-           
-        }
-        return "("+ robot.getPosition_x + robot.getPosition_y + robot.getPointer+")";
+                return "Comando inválido, com este comando o robô irá para fora do terreno!";
+            } 
+           }
+        return "("+ robot.getPosition_x +","+ robot.getPosition_y +","
+        
+        
+        + robot.getPointer+")";
     }catch (e){
         if(robot.getPosition_x<0){
-            return "Comando invalido, com este comando o robô irá para fora do terreno";
-        } else return "Ocorreu um erro na aplicação";
+            return "Comando inválido, com este comando o robô irá para fora do terreno!";
+        }else  return "Ocorreu um erro na aplicação";
     } 
     
 }
-console.log(PositionRobot(_robot,_ground,c));
